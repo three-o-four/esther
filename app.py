fe51bc98081baa22177b6e5a009005bc2762a18a
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-05-21 23:55:45 -0300

    feat: write output to file

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ url = "https://api.quotable.io/random"
 response = requests.get(url)
 parsed_response = json.loads(response.text)
 tweet = "'status=" + parsed_response.get('content') + '\n' + parsed_response.get('author')  + "'"  
-cmd = 'twurl -d {} /1.1/statuses/update.json | jq'.format(tweet)
+cmd = "twurl -d {} /1.1/statuses/update.json | jq '.' > esther-result.txt".format(tweet)
 os.system(cmd)
